3e891c63d1a5b8bc18cd4051029ba62ebbfe28f2
Eric Gu <eric.changning.gu@gmail.com>
2026-01-30 06:14:26 -0500

    feat: Add Pytest

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from main import app
+
+def test_root():
+    client = TestClient(app)
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+def test_status():
+    client = TestClient(app)
+    response = client.get("/status")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
